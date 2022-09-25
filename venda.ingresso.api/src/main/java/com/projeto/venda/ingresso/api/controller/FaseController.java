d27872c92167e3acfdbc9f9ea1a9f78083997457
silas-sousa <silas.sousa@ufu.br>
2022-09-24 23:53:23 -0300

    db configurations

---
+++ venda.ingresso.api/src/main/java/com/projeto/venda/ingresso/api/controller/FaseController.java
@@ -0,0 +1,29 @@
+package com.projeto.venda.ingresso.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RequestMapping(path = "/api/v1")
+public class FaseController {
+
+	@Autowired
+	@GetMapping("/teste")
+	public void teste() {
+		System.out.println("Teste");
+	}
+
+}
